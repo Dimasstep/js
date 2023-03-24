73be838aa6978f070303a268a4a70be13a2b294f
Dimasstep <fanzor2006@gmail.com>
2023-03-24 19:27:10 +0300

    Project JS

---
+++ demo.js
@@ -0,0 +1,29 @@
+let naminG = prompt("Как Вас зовут?", "Сюда вводим свои данные"); {
+    console.log(typeof naminG); 
+    console.log(`Пользователь представился как -${naminG}`);
+    
+}
+
+let aginG = (45); {
+    
+    prompt(`${naminG}, cколько Вам лет?`, "Сюда вводим свои данные" ); {
+        console.log(typeof aginG);
+        console.log(`Пользователь представился как ${aginG}`);
+    }
+
+
+}
+
+let statuS = (true); confirm("Сесть ли супруг/супруга?"); if (true) {
+        console.log("Да, есть жена");
+        console.log(typeof statuS);
+    } else  {
+        console.log("Нет, не женат");
+        console.log(typeof statuS);
+
+    }
+       
+
+    
+
+// console.log (`Пользователя зовут ${naminG}, его возраст составляет ${aginG}. Статус семейного положения ${statuS}`);
